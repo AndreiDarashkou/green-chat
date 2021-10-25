53584d266452eb681d3d5f7a274c60fc0d124720
AndreiDarashkou <andrei.darashkou@gmail.com>
2021-10-25 22:01:21 +0300

    fix spring boot app main method

--- src/main/java/org/green/chat/GreenChatApplication.java
+++ src/main/java/org/green/chat/GreenChatApplication.java
@@ -2,27 +2,12 @@ package org.green.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
-import java.time.Duration;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-//@SpringBootApplication
+@SpringBootApplication
 public class GreenChatApplication {
 
-    public static void main(String[] args) throws InterruptedException {
-        Flux.range(1, 100)
-                .map(it -> new Random().nextInt(5))
-                .doOnNext(val -> System.out.println("next: " + val))
-                .distinctUntilChanged()
-                .doOnNext(val -> System.out.println("distinct: " + val))
-                .subscribe();
-
-        Thread.sleep(5000);
-        //SpringApplication.run(GreenChatApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(GreenChatApplication.class, args);
     }
 
 }
