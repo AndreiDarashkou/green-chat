12f726588267e886d56aa6fef6e91e863b004797
AndreiDarashkou <andrei.darashkou@gmail.com>
2021-11-13 14:56:03 +0200

    remove embedded postgres

--- src/main/java/org/green/chat/config/FlywayConfig.java
+++ src/main/java/org/green/chat/config/FlywayConfig.java
@@ -1,11 +1,9 @@
 package org.green.chat.config;
 
-import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.flywaydb.core.Flyway;
 import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -28,26 +26,16 @@ public class FlywayConfig {
         return new FlywayMigrationInitializer(flyway, null);
     }
 
-//    @Bean
-//    public DataSource dataSource(FlywayProperties properties) {
-//        PGSimpleDataSource dataSource = new PGSimpleDataSource();
-//        dataSource.setTcpKeepAlive(true);
-//        dataSource.setUrl(properties.getUrl());
-//        dataSource.setUser(properties.getUsername());
-//        dataSource.setPassword(properties.getPassword());
-//        dataSource.setDatabaseName(properties.getDatabase());
-//
-//        return dataSource;
-//    }
-
     @Bean
-    @Primary
-    public DataSource inMemoryDS() throws Exception {
-        DataSource embeddedPostgresDS = EmbeddedPostgres.builder()
-                .setPort(5432)
-                .start().getPostgresDatabase();
+    public DataSource dataSource(FlywayProperties properties) {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setTcpKeepAlive(true);
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUser(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+        dataSource.setDatabaseName(properties.getDatabase());
 
-        return embeddedPostgresDS;
+        return dataSource;
     }
 
 }
