741d4253b1f0c4228c5bb0c1cd8b5346714e721a
AndreiDarashkou <andrei.darashkou@gmail.com>
2021-11-15 12:59:20 +0200

    use native query

--- src/main/java/org/green/chat/repository/ChatRepository.java
+++ src/main/java/org/green/chat/repository/ChatRepository.java
@@ -1,6 +1,7 @@
 package org.green.chat.repository;
 
 import org.green.chat.repository.entity.Chat;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -8,6 +9,7 @@ import reactor.core.publisher.Flux;
 @Repository
 public interface ChatRepository extends ReactiveCrudRepository<Chat, Long> {
 
+    @Query("select * from green_chat.chats where users @> array[$1]::bigint[]")
     Flux<Chat> findAllByUsersContains(long userId);
 
     Flux<Chat> findAllByUsersContainsAndGroupIsFalse(long userId);
