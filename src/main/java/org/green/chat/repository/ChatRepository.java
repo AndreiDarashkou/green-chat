9be58cda07296123ceea00f81a3075e8d8a92f25
AndreiDarashkou <andrei.darashkou@gmail.com>
2021-11-17 18:37:38 +0200

    fix chat repo query

--- src/main/java/org/green/chat/repository/ChatRepository.java
+++ src/main/java/org/green/chat/repository/ChatRepository.java
@@ -14,7 +14,7 @@ public interface ChatRepository extends ReactiveCrudRepository<Chat, Long> {
             (case when is_group then ch.name else u.username end) as name,
             (case when is_group then ch.color else u.color end) as color
         from green_chat.chats ch
-        join green_chat.users u on u.id = $1
+        join green_chat.users u on u.id = (array_remove(users, $1)::bigint[])[1]
         where users @> array[$1]::bigint[]
     """)
     Flux<Chat> findAllByUsersContains(long userId);
